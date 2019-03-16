f3256ed36d9134024dc124870e2c29571401105b
Ramazan Pala <sorusuz_cevaplar@hotmail.com>
2019-03-16 18:52:03 +0300

    Multiply matrix with rank vector in CSR format

--- p_ranking.c
+++ p_ranking.c
@@ -10,7 +10,15 @@
 #define BETA 0.8
 #define EPSILON 0.000001
 
-int readinputs(FILE *fptr, int *rowstarts, int *values, int *colindices);
+struct CSRMatrix {
+   int n_rows;
+   int n_cols;
+   int * rowstarts;
+   int * colindices;
+   float * values;
+};
+
+int readinputs(FILE *fptr, int *rowstarts, float *values, int *colindices);
 
 float ** getP(int rowstartssize, int *rowstarts, int *values, int *colindices);
 
@@ -24,205 +32,146 @@ void normalize(float ** P);
 
  int main() {
 
- 	float ** P;
- 	float * r0 = (float*)malloc(N * sizeof(float));
-	float ** resultRank = (float**)malloc(N * sizeof(float*));
+    struct CSRMatrix P;
+    //printf("Here");
+    P.rowstarts = (int*)calloc(N+1, sizeof(int));
+    P.colindices = (int*)calloc(N*N, sizeof(int));
+    P.values = (float*)calloc(N*N, sizeof(float));
+    
+    float * r = (float*)calloc(N, sizeof(float));
+    float * nextR = (float*)calloc(N, sizeof(float));
+    
+    int i, j, k, step;
+    float totalDiff = 0;
 
- 	float ** prevP = (float**)malloc(N * sizeof(float*)); // a^1 * P^1 at first
- 	float ** nextP = (float**)malloc(N * sizeof(float*)); // 0 at first
-	float ** alphaBetaP = (float**)malloc(N * sizeof(float*)); // 0.2 * 0.8 * P at first
- 	float totalDiff = 0;
- 	int i, j, k, step;
+    // read matrix file
+    FILE *fptr;
+    fptr = fopen("matrix.txt","r");
+    if(fptr == NULL)
+    {
+            printf("File not found!");
+            exit(1);
+    }
 
-	// read matrix file
-	FILE *fptr;
-	fptr = fopen("matrix.txt","r");
-	if(fptr == NULL)
-   	{
-      		printf("File not found!");
-      		exit(1);
-   	}
+    k = readinputs(fptr, P.rowstarts, P.values, P.colindices);
+    fclose(fptr);
+    
+    // construct P matrix
+    //P = getP(k, rowstarts, values, colindices);
+    //normalize(P);
+    
 
-	int * rowstarts = (int*) malloc(25*sizeof(int));
-	int * values = (int*) malloc(25*sizeof(int));
-	int * colindices = (int*) malloc(25*sizeof(int));
-	k = readinputs(fptr, rowstarts, values, colindices);
-	fclose(fptr);
-	
-	// construct P matrix
-	P = getP(k, rowstarts, values, colindices);
-	normalize(P);
-	
+    /* Some initializations */
+    for (i=0; i<N; i++) {
+        r[i] = 1.0/N;
+    }
 
- 	/* Some initializations */
- 	for (i=0; i < N; i++) {
-   		r0[i] = 1.0;
- 	}
-	for (i=0; i < N; i++) {
-		//P[i] = (float*)malloc(N * sizeof(float));
-		alphaBetaP[i] = (float*)malloc(N * sizeof(float));
-		prevP[i] = (float*)malloc(N * sizeof(float));
-		nextP[i] = (float*)malloc(N * sizeof(float));
-		resultRank[i] = (float*)malloc(N * sizeof(float));
-   		for (j=0; j < N; j++) {
-			//P[i][j] = 0.1 * ALPHA; // take this from file.txt
-			alphaBetaP[i][j] = ALPHA * BETA * P[i][j];
-			prevP[i][j] = P[i][j];
-   			nextP[i][j] = 0;
-			resultRank[i][j] = 0;
- 		}
- 	}
+    time_t t;
+    srand((unsigned) time(&t));
+    float t0,t1;
+    step = 1;
+    
+    while(1){
+        #pragma omp parallel shared(P, r, nextR) private(i, j, k)
+        {
+            t0 = omp_get_wtime();
+            // calculate nextP
+            #pragma omp for
+             for(i=0; i<N; i++){
+                 for(k=P.rowstarts[i]; k<P.rowstarts[i+1]; k++){
+                     nextR[i] = nextR[i] + ALPHA*P.values[k]*r[P.colindices[k]];
+                 }
+                 //printf("%0.6f \n", nextR[i]);
+                 nextR[i] = nextR[i] + (1-ALPHA)/N;
+                 //printf("%0.6f \n ", (1-ALPHA)/N);
+             }
 
-	time_t t;
-	srand((unsigned) time(&t));
-	float t0,t1;
- 	step = 1;
-	while(1){
-		#pragma omp parallel shared(P, r0, prevP, nextP, alphaBetaP, resultRank, t) private(i, j, k, t0, t1)
-		{
-			t0 = omp_get_wtime();
-   			// calculate nextP
-   			#pragma omp for
-			 for(i=0; i<N; i++){
-				 for(j=0; j<N ; j++){
-					 for(k=0; k<N ; k++){
-						 nextP[i][j] = nextP[i][j] + ALPHA * P[i][k] * prevP[k][j];
-					 }
-				 }
-			 }
+            // calculate totalDiff
+            #pragma omp for reduction(+:totalDiff)
+            for(i=0; i<N; i++){
+                totalDiff +=  fabs( nextR[i] - r[i] );
+            }
 
-			// calculate totalDiff
-			#pragma omp for reduction(+:totalDiff)
-			 for(i=0; i<N; i++){
-				for(k=0; k<N ; k++){
-					resultRank[i][i] += nextP[i][k]*r0[k] - prevP[i][k]*r0[k] + alphaBetaP[i][k]*r0[k];
-					totalDiff +=  fabs( nextP[i][k]*r0[k] - prevP[i][k]*r0[k] + alphaBetaP[i][k]*r0[k] );
-				}
-		        }
+            t1 = omp_get_wtime();
+            printf("Thread%d spent %f secs in the parallel region.\n", omp_get_thread_num(), t1-t0);
+        } // end of parallel section
 
-			// update alphaBetaP
-			#pragma omp for
-			 for(i=0; i<N; i++){
-				for(k=0; k<N ; k++){
-					alphaBetaP[i][k] = 0.2 * P[i][k] * alphaBetaP[i][k];
-			 	}
-			 }
+        printf("Step: %d\n", step);
+        printf("%.6f\n", totalDiff);
+        //break;
 
-			t1 = omp_get_wtime();
-			printf("Thread%d spent %f secs in the parallel region.\n", omp_get_thread_num(), t1-t0);
-   		} // end of parallel section
+        if(totalDiff <= EPSILON) break;
+        
+        // free unused heap memory
+        free(r);
+        
+        // update iteration variables
+        r = nextR;
+        totalDiff = 0;
+        step++;
+    }
 
-		printf("Step: %d\n", step);
-		printf("%.6f\n", totalDiff);
-
-   		if(totalDiff <= EPSILON) break;
-		
-		// free unused heap memory
-		for(i=0; i<N; i++){
-			free(prevP[i]);
-		}
-		
-		// update iteration variables
-   		prevP = nextP;
-		totalDiff = 0;
-   		step++;
-	}
-
-	printf("Resultant Ranks\n");
-	for(i=0; i<N ; i++){
-		printf("ranks[%d]=%.6f\n", i, resultRank[i][i] + r0[i]);
-	}
-
-	// free unused heap memory
-	for(i=0; i<N; i++){
-		free(P[i]);
-		free(prevP[i]); // nextP points to same address
-		free(alphaBetaP[i]);
-		free(resultRank[i]);
-	}
-	free(P);
-	free(prevP);
-	free(alphaBetaP);
-	free(resultRank);
-	free(r0);
-
+    printf("Resultant Ranks\n");
+    for(i=0; i<N ; i++){
+        printf("ranks[%d]=%.6f\n", i, nextR[i]);
+    }
+    
+    // free unused heap memory
+    //free(P.rowstarts);
+    //free(P.colindices);
+    //free(P.values);
+    free(r);
  }
 
-int readinputs(FILE *fptr, int *rowstarts, int *values, int *colindices){
-	char *token;
-	const char* delim = " ";
-	char * line;
-	size_t len = 0;
-    	ssize_t read = 0;
-	
-	int k = 0;
-	int l = 0;
-	int m = 0;
-	int linecount = 0;
-	int tokencount = 0;
-	while((read = getline(&line, &len, fptr)) != -1){
-		printf("%s", line);
-		token = strtok(line, delim);
-		linecount++;
-		while(token != NULL){
-			if(strcmp(token, "[") != 0 && strcmp(token, "]") != 0 && strcmp(token, "row_begin") != 0 && strcmp(token, "col_indices") != 0 && strcmp(token, "values") != 0 && strcmp(token, "=") != 0) {
-				if(linecount == 1){
-					rowstarts[k] = atoi(token);
-					k++;
-				}
-				else if(linecount == 2){
-					values[l] = atoi(token);
-					l++;
-				}
-				else if(linecount == 3){
-					colindices[m] = atoi(token);
-					m++;
-				}	
-			}
-			token = strtok(NULL, " ");
-		}
-	}
+int readinputs(FILE *fptr, int *rowstarts, float *values, int *colindices){
+    char *token;
+    const char* delim = " ";
+    char * line;
+    size_t len = 0;
+        ssize_t read = 0;
+    
+    int k = 0;
+    int l = 0;
+    int m = 0;
+    int linecount = 0;
+    int tokencount = 0;
+    while((read = getline(&line, &len, fptr)) != -1){
+        printf("%s", line);
+        token = strtok(line, delim);
+        linecount++;
+        while(token != NULL){
+            if(strcmp(token, "[") != 0 && strcmp(token, "]") != 0 && strcmp(token, "row_begin") != 0 && strcmp(token, "col_indices") != 0 && strcmp(token, "values") != 0 && strcmp(token, "=") != 0) {
+                if(linecount == 1){
+                    rowstarts[k] = atoi(token);
+                    k++;
+                }
+                else if(linecount == 2){
+                    values[l] = (float) atoi(token);
+                    l++;
+                }
+                else if(linecount == 3){
+                    colindices[m] = atoi(token);
+                    m++;
+                }   
+            }
+            token = strtok(NULL, " ");
+        }
+    }
 
-	if(line != NULL) free(line);
+    if(line != NULL) free(line);
 
-	return k;
-}
-
-float ** getP(int rowstartssize, int *rowstarts, int *values, int *colindices){
-	int j = 0;
-	int t = 0;
-	float ** P = (float**) malloc(N*sizeof(float*));
-	for(j=0; j<N ; j++){
-		P[j] = (float*) calloc(N, sizeof(float));
-	}
-	int row;
-	for(row=0; row<rowstartssize-2 ; row++){
-		int elemcount = rowstarts[row+1] - rowstarts[row];
-		for(j=t; j<t+elemcount; j++){
-			P[row][colindices[j]] = values[j];
-		}
-		t = t + elemcount;
-	}
-
-	for(j=0; j<N ; j++){
-		for(t=0; t<N ; t++){
-			printf("%.f ", P[j][t]);
-		}
-		printf("\n");
-	}
-
-	return P;
+    return k;
 }
 
 void normalize(float ** P){
-	int i,j;
-	for(i=0 ; i<N ; i++){
-		float rowsum = 0;
-		for(j=0 ; j < N ; j++){
-			rowsum += P[i][j];
-		}
-		for(j=0 ; j < N ; j++){
-			P[i][j] = ( 0.1*rowsum/N + 0.9*P[i][j] )/rowsum;
-		}
-	}
+    int i,j;
+    for(i=0 ; i<N ; i++){
+        float rowsum = 0;
+        for(j=0 ; j < N ; j++){
+            rowsum += P[i][j];
+        }
+        for(j=0 ; j < N ; j++){
+            P[i][j] = ( 0.1*rowsum/N + 0.9*P[i][j] )/rowsum;
+        }
+    }
 }
